33d3ecc5b8b280d213e20c56a586ebd3494f420b
Tony S <sanjayatony@gmail.com>
2021-02-19 14:18:29 +0700

    first call

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,28 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        povinuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="container mx-auto">
+    <Header />
+    <div class="py-4">
+      <input
+        type="search"
+        placeholder="Search products"
+        class="px-4 py-2 w-full rounded-full focus:rounded-full border b-gray-100 focus:b-gray-100 outline-none"
+      />
+    </div>
+    <div class="py-4">
+      <div class="">
+        Product
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({ $axios, $config: { consumerKey, consumerSecret } }) {
+    const prods = await $axios.$get(
+      `/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`
+    );
+    console.log(prods);
+  }
+};
 </script>
